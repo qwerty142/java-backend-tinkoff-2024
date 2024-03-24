4dc857b313120c61df9137a8c981e99600e33176
qwerty142 <62697835+qwerty142@users.noreply.github.com>
2024-03-24 23:01:45 +0300

    add_tests

--- scrapper/src/test/java/edu/java/scrapper/JpaLinkServiceTests.java
+++ scrapper/src/test/java/edu/java/scrapper/JpaLinkServiceTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(properties = "app.databaseAccessType=jpa")
@@ -30,5 +31,58 @@ public class JpaLinkServiceTests extends IntegrationTest {
             ResourceNotFoundException.class,
             () -> linkService.add(id, TEST_LINK)
         );
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void addSameLinkInChats() {
+        long chat1 = 1L;
+        long chat2 = 2L;
+        long chat3 = 3L;
+        chatService.register(chat1);
+        chatService.register(chat2);
+        chatService.register(chat3);
+
+        linkService.add(chat1, TEST_LINK);
+        linkService.add(chat2, TEST_LINK);
+        linkService.add(chat3, TEST_LINK);
+
+        assertThat(linkRepository.findAllByChatsChatId(chat1)).hasSize(1);
+        assertThat(linkRepository.findAllByChatsChatId(chat2)).hasSize(1);
+        assertThat(linkRepository.findAllByChatsChatId(chat3)).hasSize(1);
+        assertThat(linkRepository.findByUrl(TEST_LINK)).isPresent();
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void deleteNotAddedLink() {
+        long Id = 1L;
+        assertThrows(
+            ResourceNotFoundException.class,
+            () -> linkService.remove(Id, TEST_LINK)
+        );
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    void deleteLinks() {
+        long chat1 = 1L;
+        long chat2 = 2L;
+        chatService.register(chat1);
+        chatService.register(chat2);
+        linkService.add(chat1, TEST_LINK);
+        linkService.add(chat2, TEST_LINK);
+
+        linkService.remove(chat1, TEST_LINK);
+        assertThat(linkRepository.findByUrl(TEST_LINK)).isPresent();
+
+        linkService.remove(chat2, TEST_LINK);
+
+        assertThat(linkRepository.findAllByChatsChatId(chat1)).isEmpty();
+        assertThat(linkRepository.findAllByChatsChatId(chat2)).isEmpty();
+        assertThat(linkRepository.findAll()).isEmpty();
     }
 }
